3e1ad46ecd038023f0b2624d455a262dce2d841f
Steve Long <steven989@gmail.com>
2016-09-01 07:45:04 -0400

    changed email provider

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
+  address:              'smtp.sendgrid.net',
   port:                 587,
-  domain:               'gmail.com',
-  user_name:            ENV["EMAIL"],
-  password:             ENV["EMAIL_PASSWORD"],
+  domain:               'heroku.com',
+  user_name:            ENV["SENDGRID_USERNAME"],
+  password:             ENV["SENDGRID_PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
 
